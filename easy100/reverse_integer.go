77bbca2d36083409df43bbc48dbe764439d9daa5
ray1888 <alal1995@hotmail.com>
2020-10-05 15:22:39 +0800

    add(easy): reverse integer

---
+++ easy100/reverse_integer.go
@@ -0,0 +1,18 @@
+package easy100
+
+import "math"
+
+func reverse(x int) int {
+	var result int
+
+	maxValue := int(math.Pow(2, 31)) - 1
+	minValue := -1 * int(math.Pow(2, 31))
+	for x != 0 {
+		result = (x % 10) + (result * 10)
+		x = x / 10
+	}
+	if result > maxValue || result < minValue {
+		return 0
+	}
+	return result
+}
